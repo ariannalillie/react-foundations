f80c554b4fa4816e8b7fa73aaf6ed64b0792a50f
Arianna Johnson <ariannajohnson588@gmail.com>
2022-08-17 08:35:44 -0400

    you try!

--- src/lessons/js-variables.js
+++ src/lessons/js-variables.js
@@ -1,6 +1,9 @@
 // You can add javascript variables into your html by placing them
 // between curly braces
 
+// To try this create a buttonText variable and add that variable inside of a 
+// button element 
+
 export const JSVariables = () => {
   const message = "Hello";
 
