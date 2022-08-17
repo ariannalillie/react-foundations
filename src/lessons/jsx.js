d3adadc3f1eaa244466a4981537f55a5cce3ac1a
Arianna Johnson <ariannajohnson588@gmail.com>
2022-08-17 08:31:40 -0400

    what is jsx?

---
+++ src/lessons/jsx.js
@@ -0,0 +1,13 @@
+// JSX allows us to write HTML elements in JavaScript and place them in the DOM
+
+export const JSX = () => {
+  return (
+    <>
+      <div>
+        <h1>I am JSX</h1>
+        <h2>Me Too!</h2>
+      </div>
+      <button>Click me, I'm JSX</button>
+    </>
+  );
+};
